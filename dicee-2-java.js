267d562c22acc117ddefe9330df34f7e439de1d1
Abdurahmanm2020 <60320088+Abdurahmanm2020@users.noreply.github.com>
2020-03-02 13:03:49 +0200

    Update dicee-2-java.js

--- dicee-2-java.js
+++ dicee-2-java.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var randomDiceImage = "images/dicee" + randomNumber1 + ".png";
+var randomDiceImage = "dicee" + randomNumber1 + ".png";
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -9,7 +9,7 @@ image1.setAttribute("src", randomDiceImage);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomDiceImage = "images/dicee" + randomNumber2 + ".png";
+var randomDiceImage = "dicee" + randomNumber2 + ".png";
 
 var image1 = document.querySelectorAll("img")[1];
 
