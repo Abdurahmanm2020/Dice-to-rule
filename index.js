2ac35698e619ef59875c5cdac4d41dc579d7e56f
Abdurahmanm2020 <60320088+Abdurahmanm2020@users.noreply.github.com>
2020-03-02 13:07:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var randomDiceImage = "images/dice" + randomNumber1 + ".png";
+var randomDiceImage = "dice" + randomNumber1 + ".png";
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -11,7 +11,7 @@ image1.setAttribute("src", randomDiceImage);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomDiceImage = "images/dice" + randomNumber2 + ".png";
+var randomDiceImage = "dice" + randomNumber2 + ".png";
 
 var image1 = document.querySelectorAll("img")[1];
 
