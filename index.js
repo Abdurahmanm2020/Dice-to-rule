f3354735702698c2f0e6bd621ca4efd702a61a1f
Caelan <60320088+Abdurahmanm2020@users.noreply.github.com>
2020-03-05 16:38:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -30,11 +30,11 @@ document.querySelectorAll("h1")[0].innerHTML = "<img src='" + filename + "'>";*/
 
 // if (randomNumber1 > randomNumber2) {
 
-// document.querySelectorAll("h1")[0].innerHTML = ("Player 1 WINS!🚩");
+ document.querySelectorAll("h1")[0].innerHTML = ("Player 1 WINS!🚩");
 
 /* document.querySelectorAll("h1")[0].innerHTML = document.getElementById("lname");*/
 // } else if (randomNumber2 > randomNumber1) {
-//    document.querySelectorAll("h1")[0].innerHTML = ("Player 2 WINS!🚩");
+   document.querySelectorAll("h1")[0].innerHTML = ("Player 2 WINS!🚩");
 } else if (randomNumber2 === randomNumber1){
   document.querySelectorAll("h1")[0].innerHTML = ("Draw 🎌");
 };
